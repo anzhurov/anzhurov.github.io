59f018ee6b8315d6072b2f2a22d5cff64a7557fb
anzhurov <triathlete433@gmail.com>
2019-03-04 22:55:18 +0300

    fix links in slider

---
+++ js/catalog.js
@@ -0,0 +1,5 @@
+var filterContent = document.getElementById("filter");
+
+filterContent.onclick = function () {
+    category.style.display = (category.style.display === 'flex') ? 'none' : 'flex';
+};
--- js/common.js
+++ js/common.js
@@ -1,4 +1,3 @@
-var filterContent = document.getElementById("filter");
 var category = document.getElementById("category--tablet");
 var menuButton = document.querySelector(".mobile-menu");
 var menu = document.getElementById("menu__mobile");
@@ -7,8 +6,5 @@ menuButton.onclick = function () {
     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 };
 
-filterContent.onclick = function () {
-    category.style.display = (category.style.display === 'flex') ? 'none' : 'flex';
-};
 
 
