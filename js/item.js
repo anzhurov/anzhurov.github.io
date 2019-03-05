96e276a7b7fb8f4aa4ed3275cb5a38fa352b19e8
anzhurov <triathlete433@gmail.com>
2019-03-05 19:44:33 +0300

    replace main image with thumbnail

--- js/item.js
+++ js/item.js
@@ -3,9 +3,7 @@ var colorButtons = document.getElementsByClassName("item__info__color__value");
 
 function setBorderHighlightEventsOnButtons(buttons) {
     function highlightButtonBorder(buttons, target) {
-        for (let i = 0; i < buttons.length; i++) {
-            removeClassIfExists(buttons.item(i), "checked__item");
-        }
+        removeClassIfExistsFromArray(buttons, "checked__item");
         addClass(target, "checked__item");
     }
 
@@ -16,6 +14,19 @@ function setBorderHighlightEventsOnButtons(buttons) {
             if (!classRemoved) {
                 highlightButtonBorder(buttons, target);
             }
+        }
+    }
+}
+
+function replacePhotos() {
+    let thumbnailPhotos = document.getElementsByClassName("item__picture--thumb");
+    let mainPhoto = document.getElementById("item__photos--full").childNodes;
+    for (let i = 0; i < thumbnailPhotos.length; i++) {
+        thumbnailPhotos.item(i).onclick = function (event) {
+            removeClassIfExistsFromArray(thumbnailPhotos, "active__picture");
+            let target = event.target;
+            mainPhoto[1].src = target.src;
+            addClass(target, "active__picture");
         }
     }
 }
@@ -32,5 +43,12 @@ function removeClassIfExists(element, className) {
     return false;
 }
 
+function removeClassIfExistsFromArray(arrayElements, className) {
+    for (let i = 0; i < arrayElements.length; i++) {
+        removeClassIfExists(arrayElements.item(i), className);
+    }
+}
+
 setBorderHighlightEventsOnButtons(sizeButtons);
 setBorderHighlightEventsOnButtons(colorButtons);
+replacePhotos();
